71304944e0e4e471a52d86b34b7d3fba8d60fa0c
Thomas Emo <117056719+icemanexi@users.noreply.github.com>
2023-07-28 11:25:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ sudo apt install python3-gps
 sudo apt install pps-tools
 sudo systemctl enable gpsd
 sudo systemctl start gpsd
+sudo apt install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
