0844896cae60be81d46a06c2b2cd05942bd4f8a9
thomas <icemanexi@github.com>
2023-07-28 16:12:35 -0400

    as
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,8 @@ sudo systemctl enable gpsd
 sudo systemctl start gpsd
 sudo apt install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-<<<<<<< HEAD
 
 git config --global user.name "thomas"
 git config --global user.email "icemanexi@github.com"
 
-=======
 cp ~/BVEXTracker/.vimrc ~/.vimrc
->>>>>>> c46f8377e589dbb049197030abbe933d3ac20e41
